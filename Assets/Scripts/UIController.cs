58b5035255f213ffe463a69eb0c4c59ea253bc5f
kharon626 <78420479+boost-vladimir112@users.noreply.github.com>
2022-07-14 14:00:28 +0300

    UI Pause

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+   
+    public Button pauseButton;
+    public Button protectButton;
+
+    public Button resumeButton;
+    public Button restartButton;
+
+    public VisualElement pausePanel;
+
+
+    void Start()
+    {
+       
+        RootButton();
+        pauseButton.clicked += PauseGame;
+        resumeButton.clicked += ResumeGame;
+        restartButton.clicked += RestartScene;
+    }
+    void Update()
+    {
+
+    }
+
+    private void RootButton()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        pauseButton = root.Q<Button>("PauseButton");
+        protectButton = root.Q<Button>("ProtectButton");
+        resumeButton = root.Q<Button>("ResumeButton");
+        restartButton = root.Q<Button>("RestartButton");
+        pausePanel = root.Q<VisualElement>("PausePanel");
+    }
+
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+ 
+    private void OpenPausePanel()
+    {
+        pausePanel.visible = true;
+    }
+    private void ClosePausePanel()
+    {
+        pausePanel.visible = false;
+    }
+
+
+    private void PauseGame()
+    {
+        OpenPausePanel();
+        Time.timeScale = 0;
+    }
+    private void ResumeGame()
+    {
+        ClosePausePanel();
+        Time.timeScale = 1;
+    }
+
+
+
+
+
+
+
+}
