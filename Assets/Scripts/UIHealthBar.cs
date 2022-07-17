582383261e081028765b944334d27f2848cc0af7
kharon626 <78420479+boost-vladimir112@users.noreply.github.com>
2022-07-17 16:00:55 +0300

    with enemy

--- Assets/Scripts/UIHealthBar.cs
+++ Assets/Scripts/UIHealthBar.cs
@@ -6,20 +6,27 @@ using UnityEngine.UIElements;
 public class UIHealthBar : MonoBehaviour
 {
     [SerializeField] Tower _playerTower;
+    [SerializeField] Tower _enemyTower;
     public ProgressBar playerFillHP;
+    public ProgressBar enemyFillHP;
     private void SetMaxHealth()
     {
         playerFillHP.highValue = _playerTower.Health;
         playerFillHP.value = _playerTower.CurrentHealth;
+
+        enemyFillHP.highValue = _enemyTower.Health;
+        enemyFillHP.value = _enemyTower.CurrentHealth;
     }
     private void SetHealth()
     {
         playerFillHP.value = _playerTower.CurrentHealth;
+        enemyFillHP.value= _enemyTower.CurrentHealth;
     }
     private void Start()
     {
         var _root = GetComponent<UIDocument>().rootVisualElement;
         playerFillHP = _root.Q<ProgressBar>("PlayerFillHp");
+        enemyFillHP = _root.Q<ProgressBar>("EnemyFillHp");
         SetMaxHealth();
     }
     private void Update()
